38f875611aabf2e01fc3a6a36f6d12535928a49e
Jeffrey Elkner <jeff@elkner.net>
2015-05-14 17:05:04 -0400

    Initial commit.

---
+++ zipcode.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    //Invoke ZIP Code Search API
+    $("#btnSearch").click(function() {
+        zipcode = $("#searchCriteria").val();
+        var xhr = new XMLHttpRequest({mozSystem: true});
+        xhr.open("GET", "http://api.zippopotam.us/us/" + zipcode, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                var result = "";
+                var resp = JSON.parse(xhr.response);
+                var city = resp.places[0]['place name'];
+                var state = resp.places[0]['state'];
+                var longitude = resp.places[0]['longitude'];
+                var latitude = resp.places[0]['latitude'];
+                result += "<li>Latitude: " + latitude + "</li>";
+                result += "<li>Longitude: " + longitude + "</li>";
+                result += "<li>City: " + city + "</li>";
+                result += "<li>State: " + state + "</li>";
+                $("#searchResults").html(result);
+                $('#searchResults').listview('refresh');
+            }
+        }
+        xhr.send();
+    });
+});
